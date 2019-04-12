341bc4eb2abb06fa54c9e52695856730866f4906
Jiachen Chen <jiachen@winlab.rutgers.edu>
2019-04-12 17:51:45 -0400

    lsa header functions added and tested.

--- gps_i/include/gps_headers.h
+++ gps_i/include/gps_headers.h
@@ -10,8 +10,11 @@
 
 #include <gps_guid.h>
 #include <gps_na.h>
+#include <rte_byteorder.h>
 #include <rte_common.h>
 #include <rte_mbuf.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,16 +23,39 @@ extern "C" {
 #define ETHER_PROTO_TYPE 0x27c0
 #define IP_PROTO_TYPE 0x90
 
-#define GPS_TYPE_LSA 0x01
-#define GPS_TYPE_PUBLISH 0x81
-#define GPS_TYPE_SUBSCRIBE 0x82
-#define GPS_TYPE_GNRS_REQ 0xc1
-#define GPS_TYPE_GNRS_RESP 0xc2
-#define GPS_TYPE_GNRS_ASSO 0xc3
+#define GPS_PKT_TYPE_MASK_APPLICATION (0x80) // 1000 0000
+#define GPS_PKT_TYPE_MASK_GNRS (0xC0)  // 1100 0000
+
+#define GPS_PKT_TYPE_LSA 0x01
+#define GPS_PKT_TYPE_PUBLISH 0x81
+#define GPS_PKT_TYPE_SUBSCRIBE 0x82
+#define GPS_PKT_TYPE_GNRS_REQ 0xc1
+#define GPS_PKT_TYPE_GNRS_RESP 0xc2
+#define GPS_PKT_TYPE_GNRS_ASSO 0xc3
 
     struct gps_pkt_common {
         uint8_t type;
     } __rte_aligned(sizeof (uint32_t));
+
+    static __rte_always_inline uint8_t
+    gps_pkt_get_type(const void *pkt) {
+        return ((const struct gps_pkt_common *) pkt)->type;
+    }
+
+    static __rte_always_inline void
+    gps_pkt_set_type(void *pkt, uint8_t type) {
+        ((struct gps_pkt_common *) pkt)->type = type;
+    }
+
+    static __rte_always_inline bool
+    gps_pkt_is_application(const void *pkt) {
+        return (gps_pkt_get_type(pkt) & GPS_PKT_TYPE_MASK_APPLICATION) == GPS_PKT_TYPE_MASK_APPLICATION;
+    }
+
+    static __rte_always_inline bool
+    gps_pkt_is_gnrs(const void *pkt) {
+        return (gps_pkt_get_type(pkt) & GPS_PKT_TYPE_MASK_GNRS) == GPS_PKT_TYPE_MASK_GNRS;
+    }
 
     struct gps_pkt_lsa {
         struct gps_pkt_common premable;
@@ -37,6 +63,55 @@ extern "C" {
         struct gps_na intermediate_na;
         uint32_t nonce;
     } __rte_packed;
+
+    static __rte_always_inline const struct gps_na *
+    gps_pkt_lsa_get_src_na(const void *pkt) {
+        return &((const struct gps_pkt_lsa *) pkt)->src_na;
+    }
+
+    static __rte_always_inline const struct gps_na *
+    gps_pkt_lsa_get_intermediate_na(const void *pkt) {
+        return &((const struct gps_pkt_lsa *) pkt)->intermediate_na;
+    }
+
+    static __rte_always_inline uint32_t
+    gps_pkt_lsa_get_nonce(const void *pkt) {
+        return rte_be_to_cpu_32(((const struct gps_pkt_lsa *) pkt)->nonce);
+    }
+
+    static __rte_always_inline void
+    gps_pkt_lsa_set_src_na(void *pkt, const struct gps_na *src_na) {
+        gps_na_copy(&((struct gps_pkt_lsa *) pkt)->src_na, src_na);
+    }
+
+    static __rte_always_inline void
+    gps_pkt_lsa_set_intermediate_na(void *pkt, const struct gps_na *intermediate_na) {
+        gps_na_copy(&((struct gps_pkt_lsa *) pkt)->intermediate_na, intermediate_na);
+    }
+
+    static __rte_always_inline void
+    gps_pkt_lsa_set_nonce(void *pkt, uint32_t nonce) {
+        ((struct gps_pkt_lsa *) pkt)->nonce = rte_cpu_to_be_32(nonce);
+    }
+
+    static inline void
+    gps_pkt_lsa_init(void *pkt, const struct gps_na *src_na, const struct gps_na *intermediate_na, uint32_t nonce) {
+        gps_pkt_set_type(pkt, GPS_PKT_TYPE_LSA);
+        gps_pkt_lsa_set_src_na(pkt, src_na);
+        gps_pkt_lsa_set_intermediate_na(pkt, intermediate_na);
+        gps_pkt_lsa_set_nonce(pkt, nonce);
+    }
+
+    static inline char *
+    gps_pkt_lsa_format(char *buf, uint32_t size, const void *pkt) {
+        char buf_src_na[GPS_NA_FMT_SIZE], buf_intermediate_na[GPS_NA_FMT_SIZE];
+        RTE_ASSERT(gps_pkt_get_type(pkt) == GPS_PKT_TYPE_LSA);
+        snprintf(buf, size, "LSA{src_na=%s,intermediate_na=%s,nonce=0x%08x}",
+                gps_na_format(buf_src_na, GPS_NA_FMT_SIZE, gps_pkt_lsa_get_src_na(pkt)),
+                gps_na_format(buf_intermediate_na, GPS_NA_FMT_SIZE, gps_pkt_lsa_get_intermediate_na(pkt)),
+                gps_pkt_lsa_get_nonce(pkt));
+        return buf;
+    }
 
     struct gps_pkt_application {
         struct gps_pkt_common premable;
--- gps_i/test/main.c
+++ gps_i/test/main.c
@@ -5,13 +5,30 @@
  * Created on April 11, 2019, 4:43 PM
  */
 
+#define RTE_ENABLE_ASSERT
+
 #include <gps_headers.h>
 #include <gps_guid.h>
 #include <gps_na.h>
 #include <rte_common.h>
+#include <rte_debug.h>
 #include <rte_eal.h>
 #include <rte_ether.h>
 #include <rte_jhash.h>
+
+#define DEFAULT_WRAP 16
+
+static void 
+print_buf(const void *buf, uint32_t size, uint32_t wrap) {
+    uint32_t i, j;
+    for (i = 0; i < size;) {
+        printf("%04X:", i);
+        for (j = 0; i < size && j < wrap; i++, j++) {
+            printf(" %02X", ((const uint8_t *) buf)[i]);
+        }
+        printf("\n");
+    }
+}
 
 static void
 test_guid(void) {
@@ -50,35 +67,66 @@ test_na(void) {
     struct gps_na na1, na2;
     char na_format[GPS_NA_FMT_SIZE];
     char content[] = {0xde, 0xad, 0xbe, 0xef};
-    rte_memcpy(&na2.value, content, sizeof(struct gps_na));
+    rte_memcpy(&na2.value, content, sizeof (struct gps_na));
 
     printf("======%s:%d %s()======\n", __FILE__, __LINE__, __FUNCTION__);
     printf("gps_na size: %zd\n", sizeof (struct gps_na));
 
     gps_na_clear(&na1);
     printf("NA1=%s\n", gps_na_format(na_format, GPS_NA_FMT_SIZE, &na1));
-    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof(na1), 0));
+    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof (na1), 0));
 
     gps_na_set(&na1, 0x567890ab);
     printf("NA1=%s\n", gps_na_format(na_format, GPS_NA_FMT_SIZE, &na1));
-    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof(na1), 0));
-    
+    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof (na1), 0));
+
     printf("NA2=%s\n", gps_na_format(na_format, GPS_NA_FMT_SIZE, &na2));
     printf("\tcmp NA1 NA2: %d\n", gps_na_cmp(&na1, &na2));
-    printf("\tjhash NA2 %" PRIu32 "\n", rte_jhash(&na2, sizeof(na2), 0));
-    
+    printf("\tjhash NA2 %" PRIu32 "\n", rte_jhash(&na2, sizeof (na2), 0));
+
     gps_na_copy(&na1, &na2);
     printf("NA1=%s\n", gps_na_format(na_format, GPS_NA_FMT_SIZE, &na1));
     printf("\tcmp NA1 NA2: %d\n", gps_na_cmp(&na1, &na2));
-    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof(na1), 0));
+    printf("\tjhash NA1 %" PRIu32 "\n", rte_jhash(&na1, sizeof (na1), 0));
+}
+
+static void
+test_header_lsa(void) {
+
+    printf("======%s:%d %s()======\n", __FILE__, __LINE__, __FUNCTION__);
+    printf("gps_pkt_common size: %zd\n", sizeof (struct gps_pkt_common));
+    printf("gps_pkt_lsa size: %zd\n", sizeof (struct gps_pkt_lsa));
+    struct gps_pkt_lsa lsa;
+
+    struct gps_na src_na, intermediate_na;
+    char buf[1024];
+    gps_na_set(&src_na, 0x567890ab);
+    gps_na_set(&intermediate_na, 0x7890abcd);
+
+    gps_pkt_lsa_init(&lsa, &src_na, &intermediate_na, 0x90abcdef);
+    printf("pkt=%s\n", gps_pkt_lsa_format(buf, sizeof (buf), &lsa));
+    print_buf(&lsa, sizeof (lsa), DEFAULT_WRAP);
+    
+    printf("src_na comp=%d, intermediate_na comp=%d, nonce=0x%08" PRIX32 "\n", 
+            gps_na_cmp(gps_pkt_lsa_get_src_na(&lsa), &src_na),
+            gps_na_cmp(gps_pkt_lsa_get_intermediate_na(&lsa), &intermediate_na),
+            gps_pkt_lsa_get_nonce(&lsa));
+    
+    gps_pkt_lsa_set_src_na(&lsa, &intermediate_na);
+    gps_pkt_lsa_set_intermediate_na(&lsa, &src_na);
+    gps_pkt_lsa_set_nonce(&lsa, 0xdeadbeef);
+    printf("pkt=%s\n", gps_pkt_lsa_format(buf, sizeof (buf), &lsa));
+    print_buf(&lsa, sizeof (lsa), DEFAULT_WRAP);
+
+    printf("src_na comp=%d, intermediate_na comp=%d, nonce=0x%08" PRIX32 "\n", 
+            gps_na_cmp(gps_pkt_lsa_get_src_na(&lsa), &src_na),
+            gps_na_cmp(gps_pkt_lsa_get_intermediate_na(&lsa), &intermediate_na),
+            gps_pkt_lsa_get_nonce(&lsa));
 }
 
 static void
 test_headers(void) {
-    
     printf("======%s:%d %s()======\n", __FILE__, __LINE__, __FUNCTION__);
-    printf("gps_pkt_common size: %zd\n", sizeof (struct gps_pkt_common));
-    printf("gps_pkt_lsa size: %zd\n", sizeof (struct gps_pkt_lsa));
     printf("gps_pkt_application size: %zd\n", sizeof (struct gps_pkt_application));
     printf("gps_pkt_publication size: %zd\n", sizeof (struct gps_pkt_publication));
     printf("gps_pkt_subscription size: %zd\n", sizeof (struct gps_pkt_subscription));
@@ -99,6 +147,7 @@ main(int argc, char **argv) {
 
     test_guid();
     test_na();
+    test_header_lsa();
     test_headers();
 
     return 0;
